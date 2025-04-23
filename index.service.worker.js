857b57552783f4901cf8fd8c6cd8ffc76ba89305
braedenlag <braedenlag@gmail.com>
2025-04-23 16:50:07 -0400

    added sheep color picker

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745270794|1672825641';
+const CACHE_VERSION = '1745441377|8807449239';
 /** @type {string} */
 const CACHE_PREFIX = 'CrazyCattle3D-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
