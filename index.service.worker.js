1a4bebdfe498dd8b90477ed13414f2109c646e4e
braedenlag <braedenlag@gmail.com>
2025-04-25 16:34:32 -0400

    added dragon and kid beatz

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745546243|486775179';
+const CACHE_VERSION = '1745613252|7510719987';
 /** @type {string} */
 const CACHE_PREFIX = 'CrazyCattle3D-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
