103dcda06e50aa62850fca9d94bf036718ceb328
braedenlag <braedenlag@gmail.com>
2025-04-24 20:49:28 -0400

    model selector and color fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745511094|65255798';
+const CACHE_VERSION = '1745542149|2528627325';
 /** @type {string} */
 const CACHE_PREFIX = 'CrazyCattle3D-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
