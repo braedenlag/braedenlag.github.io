9c0931c609a892e658126ad50c0c28e70f9efb6a
braedenlag <braedenlag@gmail.com>
2025-04-25 17:03:52 -0400

    added watermark and such

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745613252|7510719987';
+const CACHE_VERSION = '1745615022|1128744515';
 /** @type {string} */
 const CACHE_PREFIX = 'CrazyCattle3D-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
