d12fca3a67a41eee8a37f1d7c4884459b8bbee65
braedenlag <braedenlag@gmail.com>
2025-04-25 20:38:49 -0400

    added rat character

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745615022|1128744515';
+const CACHE_VERSION = '1745627913|561485473';
 /** @type {string} */
 const CACHE_PREFIX = 'CrazyCattle3D-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
