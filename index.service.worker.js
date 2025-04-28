b9a30e40609de9ba2db8beaa0784cce739cfe327
braedenlag <braedenlag@gmail.com>
2025-04-28 15:00:17 -0400

    made resetting progress take you back to the menu

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745786047|473575516';
+const CACHE_VERSION = '1745866793|138680999';
 /** @type {string} */
 const CACHE_PREFIX = 'CrazyCattle3D-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
